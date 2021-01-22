a05c6d8e5bef46054d67eafba859005d30db4610
Mateo Puhalovic <mateo.puhalovic@five.agency>
2021-01-22 12:13:40 +0100

    Init Android app

---
+++ Android/Shopking/app/src/main/java/hr/fer/objobl/shopking/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package hr.fer.objobl.shopking.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hr.fer.objobl.shopking.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
