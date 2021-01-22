6033dfa092f196b3db671d19c1ca17692a131d14
Mateo Puhalovic <mateo.puhalovic@five.agency>
2021-01-22 13:39:30 +0100

    Add bottom navigation

--- Android/Shopking/app/src/main/java/hr/fer/objobl/shopking/view/MainActivity.kt
+++ Android/Shopking/app/src/main/java/hr/fer/objobl/shopking/view/MainActivity.kt
@@ -1,12 +1,29 @@
 package hr.fer.objobl.shopking.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import hr.fer.objobl.shopking.R
+import hr.fer.objobl.shopking.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.bottom_navigation_catalogoue_item -> true
+                R.id.bottom_navigation_shopping_list_item -> true
+                R.id.bottom_navigation_wish_list_item -> true
+                R.id.bottom_navigation_recipes_item -> true
+                R.id.bottom_navigation_information_item -> true
+                else -> false
+            }
+        }
     }
 }
