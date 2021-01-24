71c4d5eaed8bdaeaba8522ac6b33f9fdffa11916
Mateo Puhalovic <mateo.puhalovic@five.agency>
2021-01-24 17:01:06 +0100

    Add placeholder

--- Android/Shopking/app/src/main/java/hr/fer/objobl/shopking/utils/ViewExt.kt
+++ Android/Shopking/app/src/main/java/hr/fer/objobl/shopking/utils/ViewExt.kt
@@ -1,6 +1,12 @@
 package hr.fer.objobl.shopking.utils
 
 import android.widget.ImageView
+import androidx.annotation.DrawableRes
 import com.bumptech.glide.Glide
+import hr.fer.objobl.shopking.R
 
-fun ImageView.load(imageUrl: String) = Glide.with(this).load(imageUrl).into(this)
+fun ImageView.load(imageUrl: String, @DrawableRes placeholder: Int = R.drawable.app_icon_blue) = Glide
+    .with(this)
+    .load(imageUrl)
+    .placeholder(placeholder)
+    .into(this)
